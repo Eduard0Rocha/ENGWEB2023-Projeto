551a48b04cd3b41e788b61a41cb2fd9262547a1f
Eduardo Rocha <73354143+Eduard0Rocha@users.noreply.github.com>
2023-06-21 01:29:58 +0100

    Add files via upload

--- Interface/app.js
+++ Interface/app.js
@@ -41,7 +41,6 @@ app.use('/admin', adminRouter);
 app.use('/addRecurso', addRecursoRouter);
 app.use('/user', userPageRouter);
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- Interface/routes/consumidor.js
+++ Interface/routes/consumidor.js
@@ -3,7 +3,50 @@ var router = express.Router();
 
 var axios = require("axios");
 
-router.get("/", function(req,res) {
+function comentar(jwtToken, rank, comentario, file_id,res) {
+
+    axios.post("http://localhost:3000/api/comentarios",{
+
+        rank: rank,
+        comentario: comentario,
+        file_id: file_id
+    },
+    {
+        headers: {
+            authorization: "Bearer " + jwtToken
+        }
+    })
+
+    .then(r => {
+        
+        return res.redirect("/consumidor");
+    })
+
+    .catch(err => {
+
+        console.log("Erro ao adicionar comentário: " + err);
+    })
+}
+
+function apagarComentario(jwtToken, idRecurso, res) {
+
+    axios.delete("http://localhost:3000/api/comentarios/" + idRecurso, {
+
+        headers: {
+            authorization: "Bearer " + jwtToken
+        }
+    })
+    .then(data => {
+
+        return res.redirect("/consumidor");
+    })
+    .catch(err => {
+
+        return res.render("error", {error: err});
+    })
+}
+
+router.get("/", async function(req,res) {
 
     var jwtToken = req.cookies.jwt;
 
@@ -12,25 +55,109 @@ router.get("/", function(req,res) {
         return res.redirect("/login");
     }
 
-    axios.get("http://localhost:3000/api/users", {
-        headers: {
-            authorization: "Bearer " + jwtToken
-        }
-    })
-        .then(r => {
+    if (req.query.rank) {
 
-            // TODO: usar este valor
-            var user = r.data;
+        var rank = parseInt(req.query.rank);
+        var comentario = req.query.comentario;
+        var file_id = req.query.file_id;
 
-            // Nota: este nao precisa de proteção pois todos os users registados podem aceder
+        return comentar(jwtToken,rank,comentario,file_id,res);
+    }
 
-            return res.render("pagConsumidor");
+    else if (req.query.apagarcomentario) {
+
+        return apagarComentario(jwtToken,req.query.apagarcomentario,res);
+    }
+
+    else {
+
+        axios.get("http://localhost:3000/api/users", {
+            headers: {
+                authorization: "Bearer " + jwtToken
+            }
         })
+            .then(r => {
 
-        .catch(err => {
+                var user = r.data;
 
-            return res.redirect("/login");
-        })
+                // Nota: este nao precisa de proteção pois todos os users registados podem aceder
+
+                var isProdutor = (user.nivel == "Produtor" || user.nivel == "Administrador");
+                var isAdmin = user.nivel == "Administrador";
+
+                var tema = req.query.tema ? req.query.tema : "";
+                var produtor = req.query.produtor ? req.query.produtor : "";
+
+                axios.get("http://localhost:3000/api/files/consumidorPageData?tema=" + tema + "&produtor=" + produtor, {
+                    headers: {
+                        authorization: "Bearer " + jwtToken
+                    }
+                })
+
+                .then(r2 => {
+
+                    var produtores = r2.data.produtores;
+                    var temas = r2.data.temas;
+                    var noticias = r2.data.noticias;
+                    var recursos = r2.data.recursos;
+
+                    recursos.forEach(recurso => {
+                        
+                        let somaRanks = 0;
+
+                        recurso.comentarios.forEach(comentario => {
+
+                            somaRanks += comentario.rank;
+                        })
+
+                        if (recurso.comentarios == []) {
+
+                            recurso.mediaRank = null;
+                        }
+
+                        else {
+
+                            recurso.mediaRank = somaRanks / recurso.comentarios.length;
+                        }
+                    });
+
+                    recursos.forEach(recurso => {
+
+                        let hasComentario = false;
+
+                        recurso.comentarios.forEach(comentario => {
+
+                            if (comentario.userName == user.username) {
+
+                                hasComentario = true;
+                            }
+                        })
+
+                        recurso.hasComentario = hasComentario;
+                    })
+
+                    return res.render("pagConsumidor", {
+                        username: user.username, 
+                        isProdutor: isProdutor,
+                        isAdmin: isAdmin,
+                        produtores: produtores,
+                        temas: temas,
+                        noticias: noticias,
+                        recursos: recursos
+                    })
+                })
+
+                .catch(err2 => {
+
+                    return res.redirect("/login");
+                })
+            })
+
+            .catch(err => {
+
+                return res.redirect("/login");
+            })
+    }
 })
 
 module.exports = router;
